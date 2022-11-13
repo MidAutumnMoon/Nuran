a39bec05fa91a916980ad35ffed5c172ffc7814d
MidAutumnMoon <me@418.im>
2022-11-13 12:29:42 +0800

    harumi: kernel option tweaks

--- machines/harumi/hardware.nix
+++ machines/harumi/hardware.nix
@@ -7,20 +7,30 @@
 
   boot.loader.grub.mirroredBoots = [
     { devices = [ "/dev/disk/by-id/ata-TOSHIBA_MG04ACA400EY_57M8K00TF7GB" ];
-      path = "/boot";
-    }
+      path = "/boot"; }
     { devices = [ "/dev/disk/by-id/ata-TOSHIBA_MG04ACA400EY_57M7K024F7GB" ];
       path = "/boot2";
     } ];
 
-  boot.initrd.availableKernelModules =
-    [ "ahci" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+      "ahci"
+      "sd_mod"
+    ];
+
+  boot.blacklistedKernelModules = [
+      "i915"
+      "snd_hda_intel"
+    ];
+
+  boot.kernelParams = [
+      "preempt=none"
+    ];
 
   hardware.enableRedistributableFirmware = true;
 
-  powerManagement.cpuFreqGovernor = "performance";
-
   hardware.cpu.intel.updateMicrocode = true;
+
+  powerManagement.cpuFreqGovernor = "performance";
 
   services.udev.extraRules = ''
       ACTION=="add|change", KERNEL=="sd*", ATTR{queue/scheduler}="none"
