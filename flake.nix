16226cf08d5e97cb7f35a2a5e7c2ee464567cf0f
MidAutumnMoon <me@418.im>
2023-02-26 21:41:46 +0800

    flake: try new formatting

--- flake.nix
+++ flake.nix
@@ -1,93 +1,91 @@
 {
 
-  description =
-    "MidAutumnMoon's system collection, aka the Nuran.";
+description = "MidAutumnMoon's system collection, aka the Nuran.";
 
 
-  inputs = {
-      nixpkgs.url =
-        "github:NixOS/nixpkgs/nixos-unstable-small";
+inputs = {
 
-      nulib.url =
-        "github:MidAutumnMoon/Nulib";
+  nixpkgs.url =
+    "github:NixOS/nixpkgs/nixos-unstable-small";
 
-      # Some modules
+  nulib.url =
+    "github:MidAutumnMoon/Nulib";
 
-      impermanence.url =
-        "github:nix-community/impermanence";
+  # Some modules
 
-      sops-nix =
-        { url = "github:Mic92/sops-nix";
-          inputs.nixpkgs.follows = "nixpkgs";
-        };
+  impermanence.url =
+    "github:nix-community/impermanence";
 
-      home-manager =
-        { url = "github:nix-community/home-manager";
-          inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-      # Some overlays
-
-      nuclage =
-        { url = "github:MidAutumnMoon/Nuclage";
-          inputs.nixpkgs.follows = "nixpkgs";
-          inputs.nulib.follows = "nulib";
-        };
+  sops-nix =
+    { url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-
-  outputs = { self, nixpkgs, ... } @ flakes:
-
-    let
-
-      lib =
-        nixpkgs.lib.extend ( import ./lib );
-
-      overlays =
-        flakes.nuclage.totalOverlays;
-
-      config =
-        { allowUnfree = true; };
-
-      modules = with flakes;
-        ( lib.listAllModules ./nixos ) ++
-        [
-          ./nudata
-          sops-nix.nixosModule
-          impermanence.nixosModule
-          home-manager.nixosModule
-        ];
-
-      pkgsForSystems =
-        lib.importNixpkgs { inherit nixpkgs config overlays; };
-
-      machine =
-        lib.assembleSystem {
-          inherit
-            nixpkgs config overlays modules;
-          arguments =
-            { inherit flakes; };
-        };
-
-    in {
-
-      inherit lib;
-
-      nixosConfigurations."lyfua" =
-        machine { toplevel = ./machines/laptop; };
-
-      colmena.meta = {
-          nixpkgs =
-            pkgsForSystems."x86_64-linux";
-          specialArgs =
-            { inherit lib flakes; };
-        };
-
-      devShells =
-        lib.hexaShell pkgsForSystems [ "sops" "colmena" "ssh-to-age" ];
-
+  home-manager =
+    { url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    # outputs end & terminate the bracket slope
+
+  # Some overlays
+
+  nuclage =
+    { url = "github:MidAutumnMoon/Nuclage";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nulib.follows = "nulib";
+    };
+
+};
+
+
+outputs = { self, nixpkgs, ... } @ flakes: let
+
+  lib =
+    nixpkgs.lib.extend ( import ./lib );
+
+  overlays =
+    flakes.nuclage.totalOverlays;
+
+  config =
+    { allowUnfree = true; };
+
+  modules = with flakes;
+    ( lib.listAllModules ./nixos ) ++
+    [
+      ./nudata
+      sops-nix.nixosModule
+      impermanence.nixosModule
+      home-manager.nixosModule
+    ];
+
+  pkgsForSystems =
+    lib.importNixpkgs { inherit nixpkgs config overlays; };
+
+  machine =
+    lib.assembleSystem {
+      inherit
+        nixpkgs config overlays modules;
+      arguments =
+        { inherit flakes; };
+    };
+
+in {
+
+  inherit lib;
+
+  nixosConfigurations."lyfua" =
+    machine { toplevel = ./machines/laptop; };
+
+  colmena.meta = {
+      nixpkgs =
+        pkgsForSystems."x86_64-linux";
+      specialArgs =
+        { inherit lib flakes; };
+    };
+
+  devShells =
+    lib.hexaShell pkgsForSystems [ "sops" "colmena" "ssh-to-age" ];
+
+}; # outputs end & terminate the bracket slope
 
 }
 
