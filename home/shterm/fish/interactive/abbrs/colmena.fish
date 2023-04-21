7886036ff1fb30cba9b02bd82bcea5ba5dda0165
MidAutumnMoon <me@418.im>
2023-04-21 17:10:00 +0800

    home: fish: add aliases for colmena

---
+++ home/shterm/fish/interactive/abbrs/colmena.fish
@@ -0,0 +1,6 @@
+abbr -a cm "colmena"
+
+abbr -a cmbd "colmena build --evaluator streaming"
+abbr -a cma "colmena apply --evaluator streaming switch"
+abbr -a cmab "colmena apply --evaluator streaming boot"
+abbr -a cmap "colmena apply --evaluator streaming push"
