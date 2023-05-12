8418286f6a88bd72c07f02227605b780c4f18cc2
MidAutumnMoon <me@418.im>
2023-05-12 21:07:44 +0800

    nixos: network: tweak

--- nixos/network/base/default.nix
+++ nixos/network/base/default.nix
@@ -21,11 +21,17 @@ lib.mkMerge [
 
 { boot.kernel.sysctl = {
 
+  "net.core.default_qdisc" = "fq";
+
   "net.ipv4.tcp_congestion_control" = "bbr";
 
   "net.ipv4.udp_rmem_min" = 8192;
 
   "net.ipv4.udp_wmem_min" = 8192;
+
+  "net.ipv4.tcp_notsent_lowat" = 16384;
+
+  "net.ipv4.tcp_slow_start_after_idle" = 0;
 
 }; }
 
