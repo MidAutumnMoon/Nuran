7b86fb63ef0bbacb54823dd8e4abd54c783657de
MidAutumnMoon <me@418.im>
2023-05-22 23:42:29 +0800

    home: wezterm: tweak
    tweak again

--- home/shterm/wezterm/default.nix
+++ home/shterm/wezterm/default.nix
@@ -5,18 +5,16 @@ let
   xdotool = lib.getExe pkgs.xdotool;
   xprop = lib.getExe pkgs.xorg.xprop;
 
-  blurScript = pkgs.writeScript "wezterm-blur" ''
-    PID="$1"
-    ${xdotool} search -pid "$PID" -sync \
+  blurScript = pkgs.writeShellScript "wezterm-blur" ''
+    "${xdotool}" search -pid "$1" -sync \
     | xargs -I{} \
-        ${xprop} -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c \
+        "${xprop}" -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c \
         -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 \
         -id {}
   '';
 
-  blurScriptAsync = pkgs.writeScript "wezterm-blur-async" ''
-    PID="$PPID"
-    timeout 3 ${blurScript} "$PID" &
+  blurScriptAsync = pkgs.writeShellScript "wezterm-blur-async" ''
+    timeout 3 "${blurScript}" "$PPID" &
     disown
   '';
 
--- home/shterm/wezterm/wezterm.lua
+++ home/shterm/wezterm/wezterm.lua
@@ -35,6 +35,7 @@ config.font_size = 16.4
 config.line_height = 1.12
 
 config.use_cap_height_to_scale_fallback_fonts = true
+config.warn_about_missing_glyphs = false
 
 
 --
@@ -52,6 +53,8 @@ config.background = {
         opacity = 0.72,
     }
 }
+
+config.enable_wayland = false
 
 wezterm.on( "window-focus-changed", function( window, pane )
     os.execute( '@blurScriptAsync@' )
@@ -80,7 +83,6 @@ config.window_frame = {
 --
 
 config.check_for_updates = false
-config.enable_wayland = false
 config.mouse_wheel_scrolls_tabs = true
 
 
