843fc430f8cc795064c71d59e4ac1737f5ca26c6
MidAutumnMoon <me@418.im>
2023-06-12 12:24:30 +0800

    pkgs: font: iosevka: patch with nerdfont

--- packages/font/iosevka/default.nix
+++ packages/font/iosevka/default.nix
@@ -1,10 +1,49 @@
-{ iosevka }:
+{
+    stdenvNoCC,
 
-iosevka.override {
+    iosevka,
+    nerd-font-patcher,
+}:
 
-  set = "teapot";
+let
 
-  privateBuildPlan =
-    builtins.readFile ./plan.toml;
+    myIosevka = iosevka.override {
+        set = "teapot";
+        privateBuildPlan =
+            builtins.readFile ./plan.toml;
+    };
+
+in
+
+stdenvNoCC.mkDerivation {
+
+    pname = "iosevka-teapot";
+    version = myIosevka.version;
+
+    nativeBuildInputs = [
+        nerd-font-patcher
+    ];
+
+    buildCommand = ''
+        declare -r FontDir="$out/share/fonts/truetype"
+        declare -r FileList="$TMPDIR/filelist"
+        mkdir -pv "$FontDir"
+
+        find "${myIosevka}" -type f \
+            \( -name '*.ttf' -o -name '*.otf' \) \
+            -print0 \
+        > "$FileList"
+
+        xargs -r0 --max-procs="$NIX_BUILD_CORES" -I{} \
+            nerd-font-patcher \
+                --makegroups -1 \
+                --complete \
+                --adjust-line-height \
+                --careful \
+                --outputdir  "$FontDir" {} \
+        < "$FileList"
+    '';
+
+    meta = myIosevka.meta;
 
 }
