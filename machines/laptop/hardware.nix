b2da872511fa156245805be11ad63b0c434bdbd1
MidAutumnMoon <me@418.im>
2023-06-19 08:47:25 +0800

    laptop: set battery charging threshold

--- machines/laptop/hardware.nix
+++ machines/laptop/hardware.nix
@@ -2,87 +2,93 @@
 
 {
 
-  # Bluetooth
+    # Bluetooth
 
-  hardware.bluetooth.enable = true;
+    hardware.bluetooth.enable = true;
 
 
-  # Misc udev
+    # Misc udev
 
-  services.udev.packages =
-    [ pkgs.k380-fn-keys-swap ];
+    services.udev.packages = [
+        pkgs.k380-fn-keys-swap
+    ];
 
-  services.udev.extraRules = ''
-    ACTION=="add|change", KERNEL=="nvme*", ATTR{queue/scheduler}="none"
+    services.udev.extraRules = let
+        batThreshold = "85";
+        batPath = "/sys/class/power_supply/BAT?/charge_control_end_threshold";
+        batRun = "${pkgs.runtimeShell} -c 'echo ${batThreshold} > ${batPath}'";
+    in ''
+        ACTION=="add|change", KERNEL=="nvme*", ATTR{queue/scheduler}="none"
+        ACTION=="add", KERNEL=="asus-nb-wmi", RUN+="${batRun}"
     '';
 
 
-  # Sound
+    # Sound
 
-  services.pipewire.enable = true;
+    services.pipewire.enable = true;
 
-  security.rtkit.enable = true;
+    security.rtkit.enable = true;
 
-  services.pipewire = {
-      alsa.enable       = true;
-      alsa.support32Bit = true;
-      pulse.enable      = true;
+    services.pipewire = {
+        alsa.enable       = true;
+        alsa.support32Bit = true;
+        pulse.enable      = true;
     };
 
 
-  # Graphic
+    # Graphic
 
-  services.xserver = {
-      enable = false;
-      dpi    = 120;
-      videoDrivers = [ "modesetting" "nvidia" ];
+    services.xserver = {
+        enable = false;
+        dpi    = 120;
+        videoDrivers = [ "modesetting" "nvidia" ];
     };
 
-  hardware.opengl = {
-      enable     = true;
-      driSupport = true;
+    hardware.opengl = {
+        enable     = true;
+        driSupport = true;
     };
 
-  hardware.nvidia = {
-      nvidiaSettings = false;
-      modesetting.enable = true;
+    hardware.nvidia = {
+        nvidiaSettings = false;
+        modesetting.enable = true;
     };
 
-  hardware.nvidia.powerManagement = {
-      enable = true;
-      finegrained = true;
+    hardware.nvidia.powerManagement = {
+        enable = true;
+        finegrained = true;
     };
 
-  hardware.nvidia.prime = {
-      offload.enable = true;
-      amdgpuBusId = "PCI:4:0:0";
-      nvidiaBusId = "PCI:1:0:0";
+    hardware.nvidia.prime = {
+        offload.enable = true;
+        amdgpuBusId = "PCI:4:0:0";
+        nvidiaBusId = "PCI:1:0:0";
     };
 
 
-  # Touchpad
+    # Touchpad
 
-  services.xserver.libinput.enable = true;
+    services.xserver.libinput.enable = true;
 
-  services.xserver.libinput.touchpad = {
-      scrollMethod = "twofinger";
-      naturalScrolling = true;
-      clickMethod = "clickfinger";
-      accelProfile = "flat";
-      accelSpeed = "1";
+    services.xserver.libinput.touchpad = {
+        scrollMethod = "twofinger";
+        naturalScrolling = true;
+        clickMethod = "clickfinger";
+        accelProfile = "flat";
+        accelSpeed = "1";
     };
 
-  services.xserver.libinput.touchpad.additionalOptions = ''
-    Option "ScrollPixelDistance" "2"
+    services.xserver.libinput.touchpad.additionalOptions = ''
+        Option "ScrollPixelDistance" "2"
     '';
 
 
-  # Steam
+    # Steam
 
-  hardware.steam-hardware.enable = true;
+    hardware.steam-hardware.enable = true;
 
-  hardware.uinput.enable = true;
+    hardware.uinput.enable = true;
 
-  users.users."teapot".extraGroups = [ "uinput" ];
+    users.users."teapot".extraGroups = [ "uinput" ];
 
 }
