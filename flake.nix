4dd49eaee1481b844947ea94793729fdff3a15e0
MidAutumnMoon <me@418.im>
2023-06-23 11:06:49 +0800

    flake: reformat

--- flake.nix
+++ flake.nix
@@ -5,47 +5,47 @@ description = "MidAutumnMoon's system collection, aka the Nuran.";
 
 inputs = {
 
-  nixpkgs.url =
-    "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url =
+        "github:NixOS/nixpkgs/nixos-unstable-small";
 
-  nixpkgs-master.url =
-    "github:NixOS/nixpkgs/master";
+    nixpkgs-master.url =
+        "github:NixOS/nixpkgs/master";
 
-  # Some packages
+    # Some packages
 
-  colmena =
-    { url = "github:zhaofengli/colmena";
-      inputs.nixpkgs.follows = "nixpkgs";
+    colmena = {
+        url = "github:zhaofengli/colmena";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
 
-  nil-lsp =
-    { url = "github:oxalica/nil";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nil-lsp = {
+        url = "github:oxalica/nil";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
 
-  nvfetcher =
-    { url = "github:berberman/nvfetcher";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nvfetcher = {
+        url = "github:berberman/nvfetcher";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
 
-  # Some modules
+    # Some modules
 
-  impermanence.url =
-    "github:nix-community/impermanence";
+    impermanence.url =
+        "github:nix-community/impermanence";
 
-  sops-nix =
-    { url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix = {
+        url = "github:Mic92/sops-nix";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
 
-  home-manager =
-    { url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
 
-  nix-index-db =
-    { url = "github:Mic92/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-db = {
+        url = "github:Mic92/nix-index-database";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
 
 };
@@ -53,169 +53,161 @@ inputs = {
 
 outputs = { self, nixpkgs, ... } @ flakes: let
 
-  lib =
-    nixpkgs.lib.extend ( import ./lib );
+    lib = nixpkgs.lib.extend ( import ./lib );
 
-  overlays =
-    builtins.attrValues self.overlays;
+    overlays =
+        builtins.attrValues self.overlays;
 
-  config =
-    { allowUnfree = true; };
+    config = {
+        allowUnfree = true;
+    };
 
-  pkgsBrew =
-    lib.brewNixpkgs nixpkgs { inherit config overlays; };
+    pkgsBrew =
+        lib.brewNixpkgs nixpkgs { inherit config overlays; };
 
-  machine =
-    lib.assembleSystem {
-      inherit nixpkgs config overlays;
-      modules = self.nixosModules;
-      arguments =
-        { inherit flakes; };
+    machine = lib.assembleSystem {
+        inherit nixpkgs config overlays;
+        modules = self.nixosModules;
+        arguments = { inherit flakes; };
     };
 
 in {
 
-  inherit lib;
+    inherit lib;
 
-  /*
-  *
-  * Overlays & packages
-  *
-  */
+    /*
+     *
+     * Overlays & packages
+     *
+     */
 
-  overlays.nuclage =
-    import ./packages { inherit lib; };
+    overlays.nuclage =
+        import ./packages { inherit lib; };
 
-  overlays.reexport =
-    import ./packages/reexport.nix { inherit flakes; };
+    overlays.reexport =
+        import ./packages/reexport.nix { inherit flakes; };
 
-  legacyPackages =
-    let
-      pkgsBrewMaster =
-        lib.brewNixpkgs flakes.nixpkgs-master { inherit config overlays; };
-    in pkgsBrewMaster lib.id;
+    legacyPackages =
+        let
+            pkgsBrewMaster =
+                lib.brewNixpkgs flakes.nixpkgs-master { inherit config overlays; };
+        in pkgsBrewMaster lib.id;
 
 
-  /*
-  *
-  * NixOS & deploy
-  *
-  */
+    /*
+     *
+     * NixOS & deploy
+     *
+     */
 
-  nixosConfigurations = {
+    nixosConfigurations = {
+        lyfua = machine { toplevel = ./machines/laptop; };
+    };
 
-    lyfua = machine { toplevel = ./machines/laptop; };
-
-    dualka = machine { toplevel = ./machines/dualka; };
-
-  };
-
-  homeModules =
-    with flakes; [
-      nix-index-db.hmModules.nix-index
+    homeModules = with flakes; [
+        nix-index-db.hmModules.nix-index
     ] ++ ( lib.listAllModules ./home );
 
-  nixosModules =
-    with flakes; [
-      ./constants
-      sops-nix.nixosModules.default
-      impermanence.nixosModule
-      home-manager.nixosModule
+    nixosModules = with flakes; [
+        ./constants
+        sops-nix.nixosModules.default
+        impermanence.nixosModule
+        home-manager.nixosModule
     ] ++ ( lib.listAllModules ./nixos );
 
 
-  colmena = lib.adoptColmena
-    self.nixosConfigurations
-    {
-      meta = {
-        nixpkgs = pkgsBrew."x86_64-linux";
-      };
-      lyfua.deployment = {
-        allowLocalDeployment = true;
-        targetHost = "localhost";
-        targetPort = 47128;
-      };
+    colmena = lib.adoptColmena
+        self.nixosConfigurations
+        {
+            meta = {
+                nixpkgs = pkgsBrew."x86_64-linux";
+            };
+            lyfua.deployment = {
+                allowLocalDeployment = true;
+                targetHost = "localhost";
+                targetPort = 47128;
+            };
+        };
+
+
+    /*
+     *
+     * devShells
+     *
+     */
+
+    shellRecipes = {
+
+        default = self.shellRecipes.nuran;
+
+        nuran = p: with p; [
+            colmena_git
+            sops
+            ssh-to-age
+        ];
+
+        nuclage = p: with p; [
+            nvfetcher_git
+        ];
+
+        numinus = p: with p; [
+            moonscript
+            watchexec
+            rsync
+        ];
+
+        cider-bubble = p: with p; [
+            wrangler
+            mdbook
+            mdbook-toc
+            minify
+        ];
+
+        rust = p: with p; [
+            rustc
+            cargo
+            cargo-bloat
+            cargo-nextest
+            cargo-outdated
+            cargo-edit
+            clippy
+            rustfmt
+            rust-analyzer
+            stdenvTeapot.cc
+            pkg-config
+        ];
+
+        rust-gtk4 = p: with p; [
+            cairo
+            gtk4
+            gdk-pixbuf
+            pango
+            libadwaita
+        ] ++ ( self.shellRecipes.rust p );
+
+        picno = p: with p; [
+            xxHash
+            waifu2x-converter-cpp
+        ];
+
+        music = p: with p; [
+            picard
+            shntool
+            cuetools
+            flac
+        ];
+
+        kernel = p: with p; [
+            gcc
+            ncurses
+            flex
+            bison
+        ];
+
     };
 
-
-  /*
-  *
-  * devShells
-  *
-  */
-
-  shellRecipes = {
-
-    default = self.shellRecipes.nuran;
-
-    nuran = p: with p; [
-      colmena_git
-      sops
-      ssh-to-age
-    ];
-
-    nuclage = p: with p; [
-      nvfetcher_git
-    ];
-
-    numinus = p: with p; [
-      moonscript
-      watchexec
-      rsync
-    ];
-
-    cider-bubble = p: with p; [
-      wrangler
-      mdbook
-      mdbook-toc
-      minify
-    ];
-
-    rust = p: with p; [
-      rustc
-      cargo
-      cargo-bloat
-      cargo-nextest
-      cargo-outdated
-      cargo-edit
-      clippy
-      rustfmt
-      rust-analyzer
-      stdenvTeapot.cc
-      pkg-config
-    ];
-
-    rust-gtk4 = p: with p; [
-      cairo
-      gtk4
-      gdk-pixbuf
-      pango
-      libadwaita
-    ] ++ ( self.shellRecipes.rust p );
-
-    picno = p: with p; [
-      xxHash
-      waifu2x-converter-cpp
-    ];
-
-    musicy = p: with p; [
-      picard
-      shntool
-      cuetools
-      flac
-    ];
-
-    kernel = p: with p; [
-      gcc
-      ncurses
-      flex
-      bison
-    ];
-
-  };
-
-  devShells =
-    lib.brewShells pkgsBrew self.shellRecipes;
+    devShells =
+        lib.brewShells pkgsBrew self.shellRecipes;
 
 };
 
