22040796fbcc4429f5e6bff24057f4ed8b21cbc7
MidAutumnMoon <me@418.im>
2023-06-23 10:28:56 +0800

    pkgs: nginx: update patch

--- packages/network/nginx/default.nix
+++ packages/network/nginx/default.nix
@@ -1,34 +1,32 @@
 {
-  lib,
-  teapot,
-  callPackage,
-  stdenvTeapot,
+    lib,
+    teapot,
+    callPackage,
+    stdenvTeapot,
 
-  nginxMainline,
-  nginxModules,
+    nginxMainline,
+    nginxModules,
 }:
 
 let
 
-  teapotModules = [
+    teapotModules = [ ];
+
+    selectedModules = with nginxModules; [
+        brotli
+        moreheaders
     ];
 
-  selectedModules = with nginxModules; [
-      brotli
-      moreheaders
-    ];
-
-
-  removeUselessFlagsFrom = lib.subtractLists [
-      "--with-http_xslt_module"
-      "--with-http_flv_module"
-      "--with-http_mp4_module"
-      "--with-http_random_index_module"
-      "--with-http_image_filter_module"
-      "--with-http_secure_link_module"
-      "--with-http_degradation_module"
-      "--with-http_stub_status_module"
-      "--with-http_addition_module"
+    removeUselessFlagsFrom = lib.subtractLists [
+        "--with-http_xslt_module"
+        "--with-http_flv_module"
+        "--with-http_mp4_module"
+        "--with-http_random_index_module"
+        "--with-http_image_filter_module"
+        "--with-http_secure_link_module"
+        "--with-http_degradation_module"
+        "--with-http_stub_status_module"
+        "--with-http_addition_module"
     ];
 
 in
@@ -37,31 +35,32 @@ lib.onceride nginxMainline
 
 ( oldArgs: {
 
-  stdenv = stdenvTeapot;
+    stdenv = stdenvTeapot;
 
-  modules = teapotModules ++ selectedModules;
+    modules = teapotModules ++ selectedModules;
 
 } )
 
 ( oldAttrs: {
 
-  patches = oldAttrs.patches ++ [
-      ( callPackage ./ngx_http_tls_dyn_size.patch.nix {} )
+    patches =
+        oldAttrs.patches
+        ++ callPackage ./patches.nix {};
+
+    configureFlags = lib.flatten [
+        ( removeUselessFlagsFrom oldAttrs.configureFlags )
+        "--without-http_uwsgi_module"
+        "--without-http_scgi_module"
+        "--without-http_grpc_module"
+        "--without-http_memcached_module"
+        "--without-http_browser_module"
+        "--without-http_ssi_module"
     ];
 
-  configureFlags =
-    ( removeUselessFlagsFrom oldAttrs.configureFlags )
-    ++ [
-      "--without-http_uwsgi_module"
-      "--without-http_scgi_module"
-      "--without-http_grpc_module"
-      "--without-http_memcached_module"
-      "--without-http_browser_module"
-      "--without-http_ssi_module"
-    ];
-
-  preBuild = oldAttrs.preBuild or "" + ''
-    makeFlagsArray+=( CFLAGS="${ toString teapot.aggressiveOptimiz }" )
+    preBuild = oldAttrs.preBuild or "" + ''
+        makeFlagsArray+=(
+            CFLAGS="${toString teapot.aggressiveOptimiz}"
+        )
     '';
 
 } )
