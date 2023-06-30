28997f4dd0c5fa601c3d947d874c0e7e2cf18f2c
MidAutumnMoon <me@418.im>
2023-06-30 10:47:10 +0800

    home: fish: add cargo abbrs

---
+++ home/shterm/fish/interactive/abbrs/cargo.fish
@@ -0,0 +1,5 @@
+abbr -a c cargo
+abbr -a ct cargo nextest run --all-features
+abbr -a cl cargo clippy --all-features
+
+abbr -a crb cargo run --bin
--- home/shterm/fish/interactive/abbrs/common.fish
+++ home/shterm/fish/interactive/abbrs/common.fish
@@ -1,9 +1,6 @@
 abbr -a v nvim
 abbr -a s systemctl
-abbr -a c cargo
+abbr -a sc screen
+abbr -a lz lazygit
 
 abbr -a - 'cd -'
-
-abbr -a sc screen
-
-abbr -a lz lazygit
