f2fa4476b21a583d58ebed556acf06a09551c9d6
MidAutumnMoon <me@418.im>
2023-06-30 13:59:30 +0800

    laptop: setup impermanence

--- machines/laptop/environment.nix
+++ machines/laptop/environment.nix
@@ -1,7 +1,20 @@
-{
+{ lib, ... }:
 
-    sops.age.sshKeyPaths = [
-        "/etc/ssh/ssh_host_ed25519_key"
+lib.mkMerge [
+
+{ sops.age.sshKeyPaths = [
+    "/etc/ssh/ssh_host_ed25519_key"
+]; }
+
+{ environment.persistence."/persist" = {
+    hideMounts = true;
+}; }
+
+{ environment.persistence."/persist".users."teapot" = {
+    directories = [
+        { directory = ".ssh"; mode = "0700"; }
     ];
+    files = [];
+}; }
 
-}
+]
