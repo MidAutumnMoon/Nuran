e1e079f275111999875af533d77351f74f7a89d7
MidAutumnMoon <me@418.im>
2023-10-30 17:53:15 +0800

    pkgs: drop nightly neovim

--- packages/editor/neovim/wrapped.nix
+++ packages/editor/neovim/wrapped.nix
@@ -1,63 +1,65 @@
 {
-  lib,
+    lib,
 
-  callPackage,
-  symlinkJoin,
-  makeWrapper,
+    symlinkJoin,
+    makeWrapper,
 
-  name ? "neovim-nightly",
-  unwrappedNeovim ? callPackage ./nightly.nix {},
+    neovim-unwrapped,
 
-  extraPrograms ? [],
+    extraPrograms ? [],
 }:
 
 let
 
-  inherit ( lib )
-    makeBinPath
-    concatStringsSep;
+    inherit ( lib )
+        makeBinPath
+        concatStringsSep
+    ;
 
-  xdgDataDirs = concatStringsSep ":" [
-      "/run/current-system/sw/share"
+    xdgDataDirs = concatStringsSep ":" [
+        "/run/current-system/sw/share"
     ];
 
-  xdgConfigDirs = concatStringsSep ":" [
-      "/etc/xdg"
-      "/run/current-system/sw/etc/xdg"
+    xdgConfigDirs = concatStringsSep ":" [
+        "/etc/xdg"
+        "/run/current-system/sw/etc/xdg"
     ];
 
 in
 
 symlinkJoin {
 
-  inherit name;
+    name = "neovim";
 
-  paths =
-    [ unwrappedNeovim ];
+    paths = [
+        neovim-unwrapped
+    ];
 
-  nativeBuildInputs =
-    [ makeWrapper ];
+    nativeBuildInputs = [
+        makeWrapper
+    ];
 
-  postBuild = ''
-    rm -v "$out/bin/nvim"
+    postBuild = ''
+        rm -v "$out/bin/nvim"
 
-    # nvim is smart enough to figure out his real
-    # path, so no need to include the runtime in wrapper
-    rm -r "$out/share/nvim"
+        # nvim is smart enough to figure out his real
+        # path, so no need to include the runtime in wrapper
+        rm -r "$out/share/nvim"
 
-    makeWrapper \
-      "${ unwrappedNeovim }/bin/nvim" "$out/bin/nvim" \
-      --inherit-argv0 \
-      --prefix PATH ':' '${ makeBinPath extraPrograms }' \
-      --set XDG_DATA_DIRS '${ xdgDataDirs }' \
-      --set XDG_CONFIG_DIRS '${ xdgConfigDirs }'
+        makeWrapper \
+            "${neovim-unwrapped}/bin/nvim" "$out/bin/nvim" \
+            --inherit-argv0 \
+            --prefix PATH ':' '${makeBinPath extraPrograms}' \
+            --set XDG_DATA_DIRS '${xdgDataDirs}' \
+            --set XDG_CONFIG_DIRS '${xdgConfigDirs}'
 
-    ln -s "$out/bin/nvim" "$out/bin/vi"
-    ln -s "$out/bin/nvim" "$out/bin/vim"
+        ln -s "$out/bin/nvim" "$out/bin/vi"
+        ln -s "$out/bin/nvim" "$out/bin/vim"
     '';
 
-  passthru.unwrapped = unwrappedNeovim;
 
-  meta.mainProgram = "nvim";
+    passthru.unwrapped = neovim-unwrapped;
+
+    meta.mainProgram = "nvim";
 
 }
