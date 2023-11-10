77c27303d2b97bc9e4c2dfc91eb7f9809996c026
MidAutumnMoon <me@418.im>
2023-11-10 15:31:00 +0800

    pkgs: kernel: remove CachyOS patches

--- packages/kernel/vanilla/patches.nix
+++ packages/kernel/vanilla/patches.nix
@@ -5,14 +5,9 @@
 
 let
 
-  cachyPatches =
-    sources."cachyos-patches".src + "/6.5";
-
   patch =
     name: patch: { inherit name patch; };
 
 in [
-
-  ( patch "bore_sche" "${cachyPatches}/sched/0001-bore.patch" )
 
 ]
