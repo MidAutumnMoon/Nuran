cf52b447977012c2651cbb40a83a63a15d8ffff0
MidAutumnMoon <me@418.im>
2023-11-25 13:13:28 +0800

    nixos: nix: remove repl-flake flag

--- nixos/warehouse/nix/default.nix
+++ nixos/warehouse/nix/default.nix
@@ -3,60 +3,59 @@
 lib.mkMerge [
 
 {
-  nix.package = pkgs.nixVersions.stable;
+    nix.package = pkgs.nixVersions.stable;
 }
 
 
 { nix.settings = {
 
-  trusted-users = [ "root" ];
+    trusted-users = [ "root" ];
 
-  auto-optimise-store = true;
+    auto-optimise-store = true;
 
-  keep-going = true;
+    keep-going = true;
 
-  narinfo-cache-negative-ttl = 60;
+    narinfo-cache-negative-ttl = 60;
 
-  # Query cache.nixos.org first
-  substituters = lib.mkAfter [
-    "https://nuirrce.cachix.org"
-  ];
+    # Query cache.nixos.org first
+    substituters = lib.mkAfter [
+        "https://nuirrce.cachix.org"
+    ];
 
-  trusted-public-keys = [
-    "nuirrce.cachix.org-1:KQWa6ZfDkMPXeDiUpmyDhNw4CmgybPyeVklmi/1Rtqk="
-  ];
+    trusted-public-keys = [
+        "nuirrce.cachix.org-1:KQWa6ZfDkMPXeDiUpmyDhNw4CmgybPyeVklmi/1Rtqk="
+    ];
 
-  auto-allocate-uids = true;
+    auto-allocate-uids = true;
 
-  use-cgroups = true;
+    use-cgroups = true;
 
-  experimental-features = [
-    "nix-command"
-    "flakes"
-    "ca-derivations"
-    "repl-flake"
-    "auto-allocate-uids"
-    "cgroups"
-  ];
+    experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+        "auto-allocate-uids"
+        "cgroups"
+    ];
 
 }; }
 
 
 { nix.registry = {
 
-  "short" = {
-    from = { id = "p"; type = "indirect"; };
-    to = { type = "path"; path = flakes.nixpkgs; };
-  };
+    "short" = {
+        from = { id = "p"; type = "indirect"; };
+        to = { type = "path"; path = flakes.nixpkgs; };
+    };
 
 }; }
 
 
 {
-  nix.nixPath = lib.mkForce [
-    "nixpkgs=${toString flakes.nixpkgs}"
-    "nixos=${toString flakes.nixpkgs}"
-  ];
+    nix.nixPath = lib.mkForce [
+        "nixpkgs=${toString flakes.nixpkgs}"
+        "nixos=${toString flakes.nixpkgs}"
+    ];
 }
 
 ]
