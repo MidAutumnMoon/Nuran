7c87b27e783856263e09885e43b63de97b79870a
MidAutumnMoon <me@418.im>
2024-02-08 13:33:22 +0800

    pkgs: prvn-pkgs: cross-compile(ish) for hysteria
    cc
    
    u
    
    hy

--- packages/pr/prvn-pkgs/upx.nix
+++ packages/pr/prvn-pkgs/upx.nix
@@ -1,22 +1,18 @@
 {
-    upx,
+    buildPackages,
 
     writeShellApplication,
-
-    nputs ? [],
 }:
 
 writeShellApplication {
 
     name = "upx-pack";
 
-    runtimeInputs = [ upx ];
-
     text = ''
         declare -r In="$1"
         declare -r Out="$2"
 
-        upx "$In" -o "$Out" \
+        ${buildPackages.upx}/bin/upx "$In" -o "$Out" \
             --lzma
     '';
 
