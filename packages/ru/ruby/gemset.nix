a6a12466ad017e47dea91b5e101b9df29df860c5
MidAutumnMoon <me@418.im>
2024-02-18 11:03:02 +0800

    pkgs: ruby: with some gems

---
+++ packages/ru/ruby/gemset.nix
@@ -0,0 +1,12 @@
+{
+  colorize = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0dy8ryhcdzgmbvj7jpa1qq3bhhk1m7a2pz6ip0m6dxh30rzj7d9h";
+      type = "gem";
+    };
+    version = "1.1.0";
+  };
+}
