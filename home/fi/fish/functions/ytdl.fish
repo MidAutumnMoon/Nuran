9eb0dd478b812f3449cb4671ae2e422f8c5d63da
MidAutumnMoon <me@418.im>
2024-07-27 22:34:00 +0800

    home/fish: tweak ytdl to not download playlist
    YouTube is going full speed on pissing off users
    without premium lol

--- home/fi/fish/functions/ytdl.fish
+++ home/fi/fish/functions/ytdl.fish
@@ -5,6 +5,7 @@ function ytdl -d 'yt-dlp config for reasonable balance between quality and size'
     set --function VideoSpec "$argv[1]"
 
     command '${lib.getExe pkgs.yt-dlp}' \
+        --no-playlist \
         -S "quality:res,hdr,codec:av1" \
         -f "bestvideo[height<=1080]+bestaudio/best" \
         --add-metadata \
