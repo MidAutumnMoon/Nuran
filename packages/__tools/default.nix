9167d971fc9136761bb85ddbd7797094c2768e33
MidAutumnMoon <me@418.im>
2024-10-29 03:07:54 +0800

    pkgs: remove legacy nvfetcher based update script

--- packages/__tools/default.nix
+++ packages/__tools/default.nix
@@ -20,11 +20,6 @@ in {
         path = ./binary-cache-builder.rb;
     };
 
-    nvfetcher-driver = self.rubyMiner {
-        path = ./nvfetcher-driver.rb;
-        runtimeDeps = with pkgs; [ which nvfetcher ];
-    };
-
     nix-update-driver = self.rubyMiner {
         path = ./nix-update-driver.rb;
         runtimeDeps = with pkgs; [ nix-update ];
