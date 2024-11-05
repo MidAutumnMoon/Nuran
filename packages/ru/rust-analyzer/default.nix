0b1cd5b6c2df73447eaf81e42ae417e8eecf7cb5
MidAutumnMoon <me@418.im>
2024-11-06 00:03:55 +0800

    everything: update
    up
    
    pkgs: update 2024/Dec/11
    
    Diff: https://github.com/rust-lang/rust-analyzer/compare/2024-12-02...2024-12-09

--- packages/ru/rust-analyzer/default.nix
+++ packages/ru/rust-analyzer/default.nix
@@ -9,16 +9,16 @@
 rustTeapot.buildRustPackage rec {
 
     pname = "rust-analyzer";
-    version = "2024-11-04";
+    version = "2024-12-09";
 
     src = fetchFromGitHub {
         owner = "rust-lang";
         repo = pname;
         rev = version;
-        hash = "sha256-5gBDDKKwiMzR7W/b4PROLeNiXbB4ux1YDDLebaFzrmM=";
+        hash = "sha256-I1uc97f/cNhOpCemIbBAUS+CV0R7jts0NW9lc8jrpxc=";
     };
 
-    cargoHash = "sha256-VCB2pbr2i6rjCcZzXIm6/qtYj16xCvtj9XH7Risuay0=";
+    cargoHash = "sha256-2SHQJzRVou2p17WIrgBx0jOhM4Mj5BofM186H/yEK8M=";
 
 
     doCheck = false;
