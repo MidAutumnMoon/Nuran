d608e9e2336d5a411e8774bfced5c0eee476a5b8
MidAutumnMoon <me@418.im>
2025-01-06 15:39:17 +0800

    lib: make brewNixOS accept list of modules

--- lib/nixpkgs/brewNixOS.nix
+++ lib/nixpkgs/brewNixOS.nix
@@ -2,15 +2,14 @@ lib:
 
 {
 
-    # brewNixOS :: attrset -> string | attrset -> module -> nixos
+    # brewNixOS :: attrset -> string | attrset -> [ module ] -> nixos
     brewNixOS = { pkgsBrew, modules ? [], arguments ? {}, }: system: toplevel:
         lib.nixosSystem {
             specialArgs = { inherit lib; };
             modules = [
                 { nixpkgs.pkgs = pkgsBrew.${system}; }
                 { _module.args = arguments; }
-                toplevel
-            ] ++ modules;
+            ] ++ toplevel ++ modules;
         }
     ;
 
