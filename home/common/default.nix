76d75abdd8c30b21eb170afbbd90681e98304584
MidAutumnMoon <me@418.im>
2025-01-06 17:44:08 +0800

    home: clean up structure
    cc
    
    cc

---
+++ home/common/default.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+let
+
+    inherit ( config.home )
+        homeDirectory
+    ;
+
+in {
+
+    home.packages = with pkgs; [
+        file
+        ripgrep fd fastfetch
+
+        libtree
+        nix-tree
+
+        rclone
+    ];
+
+    home.sessionVariables = {
+
+        NIXPKGS_ALLOW_UNFREE = 1;
+
+        Nuran = "${homeDirectory}/Nuran";
+
+    };
+
+}
+
--- home/di/direnv/default.nix
+++
@@ -1,5 +0,0 @@
-{
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
-}
-
