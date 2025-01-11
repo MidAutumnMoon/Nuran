9a3c91379a6c0a9b13a7e8a998d526c37b22cc80
MidAutumnMoon <me@418.im>
2025-01-11 22:08:14 +0800

    lib: clean up brewShells

--- lib/nixpkgs/brewShells.nix
+++ lib/nixpkgs/brewShells.nix
@@ -2,34 +2,33 @@ lib:
 
 let
 
-  inherit ( builtins )
-    isAttrs
-    isList
-    mapAttrs
+    inherit ( lib )
+        isAttrs
+        isList
+        isDerivation
+        mapAttrs
     ;
 
-  brewer =
-    pkgs: recipe:
-      let drink = recipe pkgs; in
-      let mkShellImpl = pkgs.mkShellNoCC; in
-      if isList drink
-        then mkShellImpl { packages = drink; }
-      else if isAttrs drink
-        then mkShellImpl drink
-      else
-        drink;
+    brewer = pkgs: recipe:
+        let drink = pkgs |> recipe; in
+        let mkShellImpl = pkgs.mkShellNoCC; in
+        if isList drink then
+            mkShellImpl { packages = drink; }
+        else if isAttrs drink && !isDerivation drink then
+            mkShellImpl drink
+        else
+            drink
+    ;
 
-in
+in {
 
-{
-
-  # brewShells :: pkgsBrew -> attrset of recipes -> attrset
-  # where
-  #   pkgsBrew: somthing cooked using ./brewNixpkgs.nix
-  #   recipe: pkgs -> list | attrset | anything
-  brewShells =
-    pkgsBrew: recipes:
-      assert isAttrs recipes;
-      pkgsBrew ( pkgs: mapAttrs ( _: recipe: brewer pkgs recipe ) recipes );
+    # brewShells :: pkgsBrew -> attrset of recipes -> attrset
+    # where
+    #   pkgsBrew: somthing cooked using ./brewNixpkgs.nix
+    #   recipe: pkgs -> list | attrset | anything
+    brewShells = pkgsBrew: shellRecipes:
+        assert isAttrs shellRecipes;
+        pkgsBrew ( pkgs: mapAttrs ( _: r: brewer pkgs r ) shellRecipes )
+    ;
 
 }
