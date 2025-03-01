4791a126f236e34a9c4450ad97c8ca74a56c3985
MidAutumnMoon <me@418.im>
2025-03-02 00:49:57 +0800

    lib/eachSystem: implement using makeExtensible

--- lib/trivial/systems.nix
+++ lib/trivial/systems.nix
@@ -1,5 +1,19 @@
 lib:
 
+let
+
+    inherit ( lib )
+        isString
+        genAttrs
+        makeExtensible
+    ;
+
+    inherit ( lib.systems )
+        flakeExposed
+    ;
+
+in
+
 {
 
     # mySystems :: [ string ]
@@ -9,6 +23,22 @@ lib:
 
 
     # eachSystem :: (string -> 'a) -> { string = 'a }
-    eachSystem = lib.genAttrs lib.systems.flakeExposed;
+    eachSystem = makeExtensible ( self: {
+        # __systems :: [ string ]
+        #
+        # The list of systems triples to be generated with.
+        __systems = flakeExposed;
+
+        # appendSystems :: [ string ] -> eachSystem
+        #
+        # Handy method for appending extra systems
+        # to the predefined system list;
+        appendSystems = xs:
+            assert lib.all isString xs;
+            self.extend ( _: prev: { __systems = prev.__systems ++ xs; } )
+        ;
+
+        __functor = _: genAttrs self.__systems;
+    } );
 
 }
