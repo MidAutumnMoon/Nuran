2deeb07a9c77acb5d88a594c418f878f23a1414e
MidAutumnMoon <me@418.im>
2025-03-09 23:40:39 +0800

    ren: amd_pstate

--- machine/ren/default.nix
+++ machine/ren/default.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
@@ -107,6 +103,8 @@
         "amdgpu"
         "kvm-amd"
     ];
+
+    boot.kernelParams = [ "amd_pstate=active" ];
 
     hardware ={
         cpu.amd.updateMicrocode = true;
