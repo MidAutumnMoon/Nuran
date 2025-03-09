3bc3bf26386578eeb9cdeb855415e4527b34b24d
MidAutumnMoon <me@418.im>
2025-03-10 01:01:13 +0800

    nixos: rename secrets for root password

--- nixos/account/default.nix
+++ nixos/account/default.nix
@@ -4,13 +4,13 @@
 
     users.mutableUsers = lib.mkForce false;
 
-    sops.secrets."root_passwd" = {
-        sopsFile = ./passwd.sops.yml;
+    sops.secrets."passwd--root" = {
+        sopsFile = ./passwd--root.sops.yml;
         neededForUsers = true;
     };
 
     users.users."root" = with config; {
-        hashedPasswordFile = sops.secrets."root_passwd".path;
+        hashedPasswordFile = sops.secrets."passwd--root".path;
         openssh.authorizedKeys.keys = [ lore.pubkeys.teapot ];
     };
 
